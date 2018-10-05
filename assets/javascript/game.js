b05c41bb0348a5aeab8e407b4ac19d7ce29807be
Justin Singh <chaosdoggs@Justins-MBP.frontierlocal.net>
2018-10-04 22:18:55 -0700

    added support for disconnecting users

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,88 +15,58 @@ $(document).ready(function () {
 
 
     firebase.initializeApp(config);
-    var totalConnected = 0;
-    var playerOneStatus = false;
-    var playerOneName = "";
-    var playerTwoStatus = false;
-    var playerTwoName
-
 
     //we need to track how many users are connected so we can start 
     //building rules for our game
 
     //initalize the database
     var database = firebase.database();
-
-    //have a variable target the connections tab in the database
-    var connectionsTab = database.ref("/Connections");
-    var game = database.ref('/Game');
-
-
-    //have a variable target when someone connects
-    var whenConnected = database.ref(".info/connected");
-
-    //check if anything changed within whenConnected
-    whenConnected.on('value', function (snap) {
-        //if snap val is true
-        if (snap.val()) {
-            //write data too the connections tab
-
-            var con = connectionsTab.push({
-                playerStatus: true,
-            })
-
-            //we create another variable because we want to delete a specific one 
-            //when the user dissconnects
-            //this will remove the specific user that lef
-            con.onDisconnect().remove();
-        }
-    })
-
-    connectionsTab.on('child_added', function () {
-        totalConnected += 1;
-        if (totalConnected == 1) {
-            game.update({
-                playerOneStatus: true,
-            })
-        }
-        if (totalConnected == 2) {
-            game.update({
-                playerTwoStatus: true,
-            })
-        }
-        console.log(totalConnected);
-    })
-    connectionsTab.on('child_removed', function () {
-        totalConnected -= 1;
-        if (totalConnected < 2) {
-            game.update({
-                playerTwoStatus: false,
-            })
-        }
-        if (totalConnected < 1) {
-            game.update({
-                playerOneStatus: false,
-            })
-        }
-        console.log(totalConnected);
-    })
-
-    //this will store the users text
-    $('#username-btn').on('click', function (e) {
+    //create a global connections tab
+    var playerTab = "";
+    //a variable to store the username
+    var username = "";
+    $(document).on('click', '#username-btn', function (e) {
         e.preventDefault();
-        var usernameText = $("#username-text").val().trim();
-        console.log(usernameText);
-        game.on('value', function (snap) {
-            playerOneStatus = snap.val().playerOneStatus;
-            playerTwoStatus = snap.val().playerTwoStatus;
+        //stores the text of the username entered
+        username = $('#username-txt').val().trim();
+        //creates a new tab in the database that has the user logged in
+        playerTab = database.ref("Players/" + username);
+        //push the user setttings
+        playerTab.set({
+            username: username,
+            wins: 0,
+            losses: 0,
+            selection: "",
         })
 
-        if (playerOneStatus == true && playerTwoStatus == false) {
-            $('#player-1-status').text(usernameText);
-        }
-    })
 
+        //have a variable target when someone connects
+        var whenConnected = database.ref(".info/connected");
+
+        //check if anything changed within whenConnected
+        whenConnected.on('value', function (snap) {
+            //if snap val is true
+            if (snap.val()) {
+                //if the connecion drops, remove the player that was added
+                playerTab.onDisconnect().remove();
+            }
+        })
+
+
+
+
+        playerTab.on('value', function (snap) {
+            var inputArray = [];
+            snapshot.forEach(snap => {
+                inputArray.push(snap.val());
+                return false;
+            });
+
+            console.log(inputArray);
+        })
+
+
+    })
 
 
 })

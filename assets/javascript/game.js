6d4a2129f93aad6916d08a995be8eb55e2a79bf4
Justin Singh <justin.singh125125@gmail.com>
2018-10-06 08:03:04 -0700

    go

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,6 +21,7 @@ $(document).ready(function () {
 
     //initalize the database
     var database = firebase.database();
+    var playerData = database.ref("All-Player-Data");
     //create a global connections tab
     var playerTab = "";
     //a variable to store the username
@@ -29,51 +30,88 @@ $(document).ready(function () {
     var playerOneUsername = "";
     var playerTwoSelection = "";
     var playerTwoUsername = "";
+    var playerCount = 0;
+    var playerCountReached = false;
+    var currentPlayer = 0;
+
+
+
+
+    //this controls how many players are joined at a time
     $(document).on('click', '#username-btn', function (e) {
         e.preventDefault();
         //stores the text of the username entered
         username = $('#username-txt').val().trim();
-        //creates a new tab in the database that has the user logged in
-        playerTab = database.ref("Players/" + username);
-        //push the user setttings
-        playerTab.set({
-            username: username,
-            wins: 0,
-            losses: 0,
-            selection: "",
-        })
 
-
-        //have a variable target when someone connects
-        var whenConnected = database.ref(".info/connected");
-
-        //check if anything changed within whenConnected
-        whenConnected.on('value', function (snap) {
-            //if snap val is true
-            if (snap.val()) {
-                //if the connecion drops, remove the player that was added
-                playerTab.onDisconnect().remove();
-            }
-
-
-            alert('player one: select weapon!');
-            alert('player two: select weapon!');
-
-        })
-        $(document).on('click', '#weapons', function (e) {
-            e.preventDefault();
-            var selection = $(this).attr('data-value');
-            playerTab.update({
-                selection: selection,
+        //if we have one or two playerse
+        if (!playerCountReached) {
+            //creates a new tab in the database that has the user logged in
+            playerTab = database.ref("Players/" + username);
+            //push the user setttings
+            playerTab.set({
+                username: username,
+                wins: 0,
+                losses: 0,
+                selection: "",
             })
+
+            //have a variable target when someone connects
+            var whenConnected = database.ref(".info/connected");
+
+            //check if anything changed within whenConnected
+            whenConnected.on('value', function (snap) {
+                //if snap val is true
+                if (snap.val()) {
+                    playerTab.onDisconnect().remove();
+
+                }
+
+
+
+            })
+        }
+
+
+
+        //this function keeps track of total players
+        database.ref('Players').on('value', function (snap) {
+            playerCount = snap.numChildren();
+            console.log(playerCount);
+            if (playerCount == 2) {
+                playerCountReached = true;
+            }
+            if (playerCount == 1) {
+                playerCountReached = false;
+            }
         })
 
 
 
-        playerTab.on('value', function (snapshot) {
 
-        });
 
+
+
+    })
+    $(document).on('click', '#weapons', function (e) {
+        e.preventDefault();
+        console.log(playerCount);
+        var selection = $(this).attr('data-value');
+        if (playerCount == 1) {
+            console.log('test');
+            alert('Player One, select your weapon!');
+
+
+            playerData.update({
+                playerOneChoice: selection,
+            })
+        }
+        if (playerCount == 2) {
+
+            alert('Player Two, select your weapon!');
+            playerData.update({
+                playerTwoChoice: selection,
+            })
+        }
 
 
     })
